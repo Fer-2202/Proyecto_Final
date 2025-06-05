e4b99e52d4d6e04d2f1005ff52e0c9228b0b8921
Fer-2202 <moyafer779@gmail.com>
2025-06-05 14:46:12 -0600

    Añadir informacion a la pagina de exhibiciones

--- frontend/src/components/home/exhibiciones-y-servicios/MarineExhibit.jsx
+++ frontend/src/components/home/exhibiciones-y-servicios/MarineExhibit.jsx
@@ -3,13 +3,21 @@
 import * as Tabs from '@radix-ui/react-tabs';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Cocodrilo from './../../../assets/img/Figuras_turismo/Figura_turismo_1.jpg';
+import Tortuga_T from './../../../assets/img/Figuras_turismo/Figura_turismo_2.jpg';
+import Caiman from './../../../assets/img/Figuras_turismo/Figura_turismo_3.jpg';
+import IslaDelCoco from './../../../assets/img/Figuras_turismo/Figura_turismo_4.jpg';
+import IslaDelCoco2 from './../../../assets/img/Figuras_turismo/Figura_turismo_5.jpg';
+import IslaDelCoco3 from './../../../assets/img/Figuras_turismo/Figura_turismo_6.jpg';
+import Acuario from './../../../assets/img/Figuras_turismo/Figura_turismo_7.jpeg';
+import Acuario2 from './../../../assets/img/Figuras_turismo/Figura_turismo_8.jpeg';
+import Raya from './../../../assets/img/Figuras_turismo/Figura_turismo_9.jpg';
+import Raya2 from './../../../assets/img/Figuras_turismo/Figura_turismo_10.jpg';
+import Tortuga_M from './../../../assets/img/Figuras_turismo/Figura_turismo_11.jpg';
+import Tortuga_M2 from './../../../assets/img/Figuras_turismo/Figura_turismo_12.jpg';
+import Tiburon from './../../../assets/img/Figuras_turismo/Figura_turismo_13.jpg';
+import Tiburon2 from './../../../assets/img/Figuras_turismo/Figura_turismo_14.jpg';
 
-import ReptileImage from './../../../assets/img/Figuras_turismo/Figura_turismo_1.jpg';
-import IslaDelCocoImage from './../../../assets/img/Figuras_turismo/Figura_turismo_4.jpg';
-import AquariumImage from './../../../assets/img/Figuras_turismo/Figura_turismo_8.jpeg';
-import RayImage from './../../../assets/img/_MG_0086.jpg';
-import TurtleImage from './../../../assets/img/_MG_0086.jpg';
-import SharkImage from './../../../assets/img/_MG_0086.jpg';
 
 export default function MarineExhibit() {
   return (
@@ -49,7 +57,7 @@ export default function MarineExhibit() {
               'Ambos forman parte de nuestros pacientes residentes del centro de rescate',
               'Podrás observar diferentes especies de tortugas terrestres y así generar conciencia para la tenencia responsable de animales silvestres',
             ]}
-            images={[ReptileImage, ReptileImage]}
+            images={[Cocodrilo, Caiman, Tortuga_T]}
           />
         </Tabs.Content>
 
@@ -58,15 +66,16 @@ export default function MarineExhibit() {
           <Section
             title="Exhibición Isla del Coco"
             description={[
-              'Una ventana al ecosistema marino de la famosa Isla del Coco.',
-              'Descubre la biodiversidad única de este parque nacional patrimonio natural de la humanidad.',
+              'Te invitamos a vivir una experiencia única en nuestra exhibición “Travesía por la Isla del Coco”. Explora la riqueza natural y la historia de esta emblemática isla a través de una detallada maqueta y un vídeo que muestra su impresionante biodiversidad.',
             ]}
             facts={[
-              'La Isla del Coco alberga más de 200 especies de peces.',
-              'Es un sitio clave para la investigación marina.',
-              'Sus aguas son hogar de tiburones martillo y mantarrayas gigantes.',
+              'Te invitamos a conocer la Isla del Coco, uno de los tesoros naturales más valiosos de Costa Rica',
+              'En nuestra exhibición “Travesía por la Isla del Coco”, descubrirás la extraordinaria biodiversidad de este Parque Nacional',
+              'Declarado Patrimonio Natural de la Humanidad por su importancia ecológica',
+              'A través de una maqueta interactiva, podrás observar cómo transcurre un día y una noche en la isla', 
+              'Apreciando sus paisajes y hábitats. Además, un video inmersivo que mostrará las especies más emblemáticas que habitan este paraíso natural',
             ]}
-            images={[IslaDelCocoImage, IslaDelCocoImage]}
+            images={[IslaDelCoco, IslaDelCoco2, IslaDelCoco3]}
           />
         </Tabs.Content>
 
@@ -75,14 +84,12 @@ export default function MarineExhibit() {
           <Section
             title="Acuarios del Parque"
             description={[
-              'Nuestros acuarios presentan ecosistemas marinos costeros y de arrecifes.',
-              'Explora la vida submarina de Costa Rica en tanques diseñados para replicar hábitats naturales.',
+              'En nuestro acuario vas a encontrar más de 20 peceras con diferentes especies del Pacifico, además de la pecera de agua salada más grande de Costa Rica en exhibición.',
             ]}
             facts={[
-              'Albergan más de 50 especies de peces.',
-              'Contamos con programas de reproducción en cautiverio.',
+              'Nuestro acuario está orientado a la fauna marina, donde se fomente la educación ambiental, la sensibilización y la conservación de la vida silvestre.',
             ]}
-            images={[AquariumImage, AquariumImage]}
+            images={[Acuario, Acuario2]}
           />
         </Tabs.Content>
 
@@ -91,15 +98,14 @@ export default function MarineExhibit() {
           <Section
             title="Exhibición de Rayas"
             description={[
-              'Observa de cerca diferentes especies de rayas y conoce sus curiosas formas de desplazamiento.',
-              'Participa en sesiones interactivas para aprender más sobre ellas.',
+              'En nuestro estanque de rayas tendrás la oportunidad única de conocer de cerca a estas asombrosas criaturas marinas. ¿Conoces la diferencia entre rayas y manta rayas?',
             ]}
             facts={[
-              'Las rayas pueden detectar campos eléctricos en el agua.',
-              'Algunas especies son venenosas.',
-              'Su esqueleto está formado por cartílago, no huesos.',
+              'Las rayas son animales tranquilos, que se deslizan por el agua con movimientos suaves',
+              'Observarlas de cerca es una experiencia inolvidable, ideal tanto para quienes visitan por primera vez como para amantes del mundo marino',
+              '¿Sabías que muchas personas confunden a las rayas con las mantarrayas? Aunque a simple vista puedan parecer similares, en realidad son especies diferentes con características únicas.'
             ]}
-            images={[RayImage, RayImage]}
+            images={[Raya, Raya2]}
           />
         </Tabs.Content>
 
@@ -108,15 +114,15 @@ export default function MarineExhibit() {
           <Section
             title="Exhibición de Tortugas"
             description={[
-              'Conoce el ciclo de vida de las tortugas marinas y las amenazas que enfrentan.',
-              'Participa en nuestros programas de conservación y liberación de tortugas.',
+              'Ven a descubrir las tortugas marinas en el Parque Marino del Pacifico.',
             ]}
             facts={[
-              'Las tortugas marinas pueden vivir más de 80 años.',
-              'Las crías enfrentan numerosos depredadores.',
-              'Varias especies están en peligro crítico de extinción.',
+              'Las tortugas marinas son unos de los seres más fascinantes y antiguos de nuestro planeta',
+              'Con sus habilidades excepcionales y su increíble resistencia',
+              'Desde su capacidad para navegar miles de kilómetros hasta su sorprendente longevidad',
+              '¡Conoce y aprende cómo podemos protegerlas para garantizar su supervivencia en nuestros océanos!'
             ]}
-            images={[TurtleImage, TurtleImage]}
+            images={[Tortuga_M2, Tortuga_M]}
           />
         </Tabs.Content>
 
@@ -125,15 +131,14 @@ export default function MarineExhibit() {
           <Section
             title="Exhibición de Tiburones"
             description={[
-              'Desmitifica la imagen de los tiburones y aprende su papel esencial en los océanos.',
-              'Nuestros tanques permiten observar de manera segura distintas especies de tiburones.',
+              '¡Conoce más sobre los tiburones Nodriza junto a nosotros! ',
             ]}
             facts={[
-              'Los tiburones han existido por más de 400 millones de años.',
-              'Algunas especies nunca dejan de nadar.',
-              'Son clave para mantener el equilibrio de los ecosistemas marinos.',
+              'El Tiburón gata nodriza (Ginglymostoma cirratum) son una especie caracterizada por tener un comportamiento tranquilo',
+              'El nombre "nodriza" se debe a su forma de cuidar a sus crías, y "gata" a las protuberancias en su morro, que recuerdan a los bigotes de un gato',
+              'Vive en una variedad de hábitat costeros poco profundos como aguas insulares, arrecifes rocosos o coralinos, áreas arenosas, entre otras, desde la zona entre mareas hasta los 15 metros. ',
             ]}
-            images={[SharkImage, SharkImage]}
+            images={[Tiburon, Tiburon2]}
           />
         </Tabs.Content>
       </Tabs.Root>
