ebb897ea1dfac4e50ebb92633a675e1203f9cc6a
scriptvg <velezalan34@gmail.com>
2025-06-04 19:43:29 -0600

    Signal para eliminar imagenes del perfil

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import PurchaseOrders, UserProfile
@@ -15,3 +15,9 @@ def generate_qr_after_order_created(sender, instance, created, **kwargs):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     profile, _ = UserProfile.objects.get_or_create(user=instance)
     profile.save()
+
+# Eliminar imagen de perfil cuando se elimina un UserProfile
+@receiver(post_delete, sender=UserProfile)
+def delete_profile_picture_on_profile_delete(sender, instance, **kwargs):
+    if instance.profile_picture:
+        instance.profile_picture.delete(save=False)
