7d09fd4d10e5983378a2f02dc67db0aaf0347fcf
scriptvg <velezalan34@gmail.com>
2025-06-05 14:18:57 -0600

    Convertir componente de ExhibitIntro.jsx a un componente reciclable

--- frontend/src/components/home/exhibiciones-y-servicios/Exhibicion.jsx
+++ frontend/src/components/home/exhibiciones-y-servicios/Exhibicion.jsx
@@ -8,7 +8,7 @@ function Exhibicion() {
     return (
         <div className='flex flex-col items-center mt-23'>
             <QuienesSomos title={"Exhibicion"} description={"Descubre la biodiversidad de Costa Rica "} img={Tanque} />
-            <ExhibitIntro />
+            <ExhibitIntro description={"El Parque Marino Central del Pacifico Sur cuenta con diversas exhibiciones que le permitirán conocer las riquezas de la biodiversidad marina de Costa Rica. Desde la fauna marina, pasando por torutgas marinas, tiburones y rayarios, nuestras exhibiciones te ofrecen una experiencia educativa y entretenida."} title={"Nuestras Exhibiciones"} />
             <MarineExhibit />
 
         </div>
