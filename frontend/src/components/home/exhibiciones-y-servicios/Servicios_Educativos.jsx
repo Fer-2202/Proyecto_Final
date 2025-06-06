4e4bae25210ad6f6fd06ec0302cbedf12e34bef9
scriptvg <velezalan34@gmail.com>
2025-06-06 13:27:08 -0600

    [Build]: Crear un componente para las cards   de la pagina de (Servicios_Educativos.jsx)

--- frontend/src/components/home/exhibiciones-y-servicios/Servicios_Educativos.jsx
+++ frontend/src/components/home/exhibiciones-y-servicios/Servicios_Educativos.jsx
@@ -4,6 +4,7 @@ import contaminacion from './../../../assets/img/contaminacion_1.jpg'
 import ExhibitIntro from './ExhibitIntro'
 import MarineExhibit from './MarineExhibit'
 import educationData from './educationData'
+import EducationPrograms from './EducationPrograms'
 
 function Servicios_Educativos() {
   return (
@@ -11,6 +12,7 @@ function Servicios_Educativos() {
       <QuienesSomos title={"Servicios Educativos"} description={"Programas educativos para los jovenes"} img={contaminacion} />
       <ExhibitIntro description={"El Parque Marino ofrece una variedad de talleres educativos diseñados para crear conciencia y promover la protección de los ecosistemas marino-costeros, el objetivo de estos talleres es que los niños y jóvenes aprendan jugando."} title={"Nuestros Servicios Educativos"}/>
       <MarineExhibit data={educationData} />
+      <EducationPrograms />
     </div>
   )
 }
